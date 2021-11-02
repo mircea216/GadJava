0c679696fd67bf64447e168640aa1dc8cca93ef4
mircea216 <panaitemircea378@yahoo.com>
2021-11-03 00:53:13 +0200

    CC1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,13 @@
 package com.company;
 
+import com.company.codeChallenges.CodeChallenge1;
+
 public class Main {
 
     public static void main(String[] args) {
         Exercises ex = new Exercises();
         ex.arrays();
-        System.out.println("Atelierul Digital Google!");
+        CodeChallenge1 codeChallenge1 = new CodeChallenge1();
+        codeChallenge1.printNumbers();
     }
 }
---
+++ src/com/company/codeChallenges/CodeChallenge1.java
@@ -0,0 +1,17 @@
+package com.company.codeChallenges;
+
+public class CodeChallenge1 {
+    public void printNumbers() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 != 0) {
+                System.out.print("Fizz ");
+            } else if (i % 5 == 0 && i % 3 != 0) {
+                System.out.print("Buzz ");
+            } else if (i % 15 == 0) {
+                System.out.print("FizzBuzz ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
